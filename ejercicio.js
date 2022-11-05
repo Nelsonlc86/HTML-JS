91760b595be001e42a78a558fc9d9e76d3417ade
Nelson Luna <105686055+Nelsonlc86@users.noreply.github.com>
2022-11-04 19:23:10 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,9 @@
+function mostrarLista() {
+document.getElementById("contenido").innerHTML=("<ul><li>Juan</li><li>Nelson</li><li>Diana</li><li>Luisa</li><li>Pedro</li></ul>")
+}
+
+
+
+function multiply(){
+    
+}
